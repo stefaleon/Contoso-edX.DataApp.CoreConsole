d6327770f57b80e68e7ab6789e4d424c6f6aa6c6
stefaleon <stefaleon@gmail.com>
2017-05-29 15:00:24 +0300

    Add project files.

---
+++ edX.DataApp.CoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace edX.DataApp.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
